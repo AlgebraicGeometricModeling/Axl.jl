1d1e7f00d5fa3233891c3181e4667579f982ad35
mourrain <bernard.mourrain@inria.fr>
2019-03-16 22:18:22 +0100

    install

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,6 @@
-using Pkg; try using SemiAlgebraicTypes
+using Pkg
+
+try using SemiAlgebraicTypes
 catch;
     Pkg.clone("https://gitlab.inria.fr/AlgebraicGeometricModeling/SemiAlgebraicTypes.jl.git")
 end
@@ -8,6 +10,7 @@ catch;
     println("-- axl does not seems to be installed")
     println("-- It can be installed with conda: \"conda install axl -c axel-distrib\"")
     println("-- see http://axl.inria.fr/installation.html")
+
 #    using Conda
 #    Conda.add_channel("axel-distrib")
 #    Conda.add("axl")
