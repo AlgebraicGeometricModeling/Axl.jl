88a5eb519a3414f71a073151ba22d5c58dde2668
mourrain <bernard.mourrain@inria.fr>
2021-01-15 13:31:42 +0100

    update project file

--- src/axlprint.jl
+++ src/axlprint.jl
@@ -29,13 +29,6 @@ function axlprint(io::IO, c::C, idt::Int64=0) where {C<:Real}
     print(io, c)
 end
 
-# function axlprint(io::IO, L::Vector{C}, idt::Int64=0) where {C <: Real}
-#     indent(io, idt)
-#     for l in L
-#         axlprint(io, l,1)
-#     end
-# end
-#
 function axlprint(io::IO, L::Vector, idt::Int64=0)
     for l in L
         axlprint(io, l,idt)
@@ -57,5 +50,8 @@ function axlprintattr(io::IO, M)
     end
     if M[:size] !=0
         print(io, " size=\"", M[:size],"\"")
+    end
+    if length(M[:name]) !=0
+        print(io, " name=\"", M[:name], "\"")
     end
 end
