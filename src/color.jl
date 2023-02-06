7b3ee014ff73742aa2f0692879201b9c850c807a
mourrain <bernard.mourrain@inria.fr>
2023-02-06 16:41:18 +0100

    clean color + rename docs

--- src/color.jl
+++ src/color.jl
@@ -1,17 +1,4 @@
-export Color, DirField, dirfield, DistField, distfield
-"""
-Color represented by the `r`, `g`, `b` components (`Int64` between 0 and 255)  and a transparency component `t` (`Float64` between 0.0 and 1.0).
-"""
-mutable struct Color
-    r::Int64
-    g::Int64
-    b::Int64
-    t::Float64
-
-    function Color(r::Int64, g::Int64, b::Int64, t::Float64=1.0)
-        new(r,g,b,t)
-    end
-end
+export DirField, dirfield, DistField, distfield
 
 Reader["color"] = function(obj::EzXML.Node)
     @assert haskey(obj,"color") "No color attribute"
